eea28d650d00e2cde7b085611909a440beb419e4
VSVenkat <venkat.vallam@gmail.com>
2016-07-04 11:38:14 +0530

    added GenericTest.java

---
+++ DataBaseProject/src/org/database/GenericTest.java
@@ -0,0 +1,10 @@
+package org.database;
+
+public class GenericTest {
+	
+	public void myhello()
+	{
+		System.out.println("this is test of the myhello method");
+	}
+
+}
--- DataBaseProject/src/org/database/data.java
+++ DataBaseProject/src/org/database/data.java
@@ -4,6 +4,8 @@ public class data {
 	
 	public static void main(String args[])
 	{
+		GenericTest gt = new GenericTest();
+		gt.myhello();
 		System.out.println("Data base connection .....established");
 	}
 
